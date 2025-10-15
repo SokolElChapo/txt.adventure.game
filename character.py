99651647eeefdf3515237ee806ca8ce9d2aea886
SokolElChapo <koli1421@hotmail.com>
2025-10-15 20:33:30 +0200

    aa
    dd

---
+++ character.py
@@ -0,0 +1,24 @@
+def player_character (player):
+    player_stats = {
+    "HP": 70,
+    "MP": 45,
+    "ATK": 7,
+    "DEF": 15
+}
+
+def enemy_goblin (goblin):
+    goblin_stats = {
+    "HP": 30,
+    "MP": 12,
+    "ATK": 14,
+    "DEF": 8
+
+}
+
+def enemy_general_goblin (general_goblin):
+    general_goblin_stats = {
+        "HP": 100,
+        "MP": 21,
+        "ATK": 27,
+        "DEF": 18
+}
